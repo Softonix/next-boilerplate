5822a6abda2c7d531af1a3de9342aee18f5776a6
Andriy Vasylytsya <andriy.vasylytsya@softonix.org>
2023-06-18 14:19:55 +0300

    (chore): updated code to useMemo()

--- src/pages/react-vs-vue/index.page.tsx
+++ src/pages/react-vs-vue/index.page.tsx
@@ -3,17 +3,16 @@ export default function ReactVsVue () {
   const [currentUser, setCurrentUser] = useState({ firstName: '', lastName: '' })
 
   function onSubmitForm () {
-    setList([...list, currentUser])
+    setList(prevList => [...prevList, currentUser])
     setCurrentUser({ firstName: '', lastName: '' })
 
     // Log new list length
     console.log('New list length is:', list.length)
   }
 
-  const [lastUser, setLastUser] = useState('')
-  useEffect(() => {
-    const lastUser = list.slice(-1)[0]
-    setLastUser(`Last user is: ${lastUser.firstName} | ${lastUser.lastName}`)
+  const lastUser = useMemo(() => {
+    const lastUser = list[list.length - 1]
+    return `Last user is: ${lastUser.firstName} | ${lastUser.lastName}`
   }, [list])
 
   return (
@@ -40,16 +39,16 @@ export default function ReactVsVue () {
             className="border border-gray-400 rounded-sm px-2"
             placeholder="First name"
             value={currentUser.firstName}
-            onInput={(e: any) => (setCurrentUser({ firstName: e.target.value, lastName: currentUser.lastName })) }
+            onInput={(e: any) => (setCurrentUser(prevUser => ({ ...prevUser, firstName: e.target.value }))) }
           />
           <input
             className="border border-gray-400 rounded-sm px-2"
             placeholder="Last name"
             value={currentUser.lastName}
-            onInput={(e: any) => (setCurrentUser({ firstName: currentUser.firstName, lastName: e.target.value })) }
+            onInput={(e: any) => (setCurrentUser(prevUser => ({ ...prevUser, lastName: e.target.value }))) }
           />
 
-          <button onClick={onSubmitForm} disabled={!currentUser.firstName}>Add me</button>
+          <button disabled={!currentUser.firstName} onClick={onSubmitForm}>Add me</button>
         </div>
       </div>
     </Layout>
